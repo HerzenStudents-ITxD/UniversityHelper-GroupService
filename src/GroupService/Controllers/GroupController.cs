cf43e5b057dc0a76a4e4e07655b521064cce2ce2
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2023-11-21 22:01:34 +0300

    Add base structure of service

---
+++ src/GroupService/Controllers/GroupController.cs
@@ -0,0 +1,48 @@
+﻿using UniversityHelper.GroupService.Business.Commands.Feedback.Interfaces;
+using UniversityHelper.GroupService.Models.Dto;
+using UniversityHelper.GroupService.Models.Dto.Models;
+using UniversityHelper.GroupService.Models.Dto.Requests;
+using UniversityHelper.GroupService.Models.Dto.Requests.Filter;
+using UniversityHelper.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace UniversityHelper.GroupService.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class GroupController : ControllerBase
+{
+  [HttpGet("get")]
+  public async Task<OperationResultResponse<GroupResponse>> GetAsync(
+    [FromQuery] Guid groupId,
+    [FromServices] IGetGroupCommand command)
+  {
+    return await command.ExecuteAsync(groupId);
+  }
+
+  [HttpGet("find")]
+  public async Task<FindResultResponse<GroupInfo>> FindAsync(
+    [FromQuery] FindGroupsFilter filter,
+    [FromServices] IFindGroupsCommand command)
+  {
+    return await command.ExecuteAsync(filter);
+  }
+
+  [HttpPost("create")]
+  public async Task<OperationResultResponse<Guid?>> CreateAsync(
+    [FromBody] CreateGroupRequest request,
+    [FromServices] ICreateGroupCommand command)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpPut("edit")]
+  public async Task<OperationResultResponse<bool>> EditGroupAsync(
+    [FromBody] EditGroupRequest request,
+    [FromServices] IEditGroupCommand command)
+  {
+    return await command.ExecuteAsync(request);
+  }
+}
---
+++ src/GroupService/Startup.cs
@@ -0,0 +1,150 @@
+﻿using HealthChecks.UI.Client;
+using UniversityHelper.GroupService.Data.Provider.MsSql.Ef;
+using UniversityHelper.GroupService.Models.Dto.Configurations;
+using UniversityHelper.Core.BrokerSupport.Configurations;
+using UniversityHelper.Core.BrokerSupport.Extensions;
+using UniversityHelper.Core.BrokerSupport.Helpers;
+using UniversityHelper.Core.BrokerSupport.Middlewares.Token;
+using UniversityHelper.Core.Configurations;
+using UniversityHelper.Core.EFSupport.Extensions;
+using UniversityHelper.Core.EFSupport.Helpers;
+using UniversityHelper.Core.Extensions;
+using UniversityHelper.Core.Middlewares.ApiInformation;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+
+namespace UniversityHelper.GroupService;
+
+public class Startup : BaseApiInfo
+{
+  public const string CorsPolicyName = "LtDoCorsPolicy";
+
+  private readonly RabbitMqConfig _rabbitMqConfig;
+  private readonly BaseServiceInfoConfig _serviceInfoConfig;
+
+  private IConfiguration Configuration { get; }
+
+  public Startup(IConfiguration configuration)
+  {
+    Configuration = configuration;
+
+    _rabbitMqConfig = Configuration
+      .GetSection(BaseRabbitMqConfig.SectionName)
+      .Get<RabbitMqConfig>();
+
+    _serviceInfoConfig = Configuration
+      .GetSection(BaseServiceInfoConfig.SectionName)
+      .Get<BaseServiceInfoConfig>();
+
+    Version = "2.0.2.0";
+    Description = "GroupService is an API that intended to work with groups and subgroups.";
+    StartTime = DateTime.UtcNow;
+    ApiName = $"UniversityHelper - {_serviceInfoConfig.Name}";
+  }
+
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddCors(options =>
+    {
+      options.AddPolicy(
+        CorsPolicyName,
+        builder =>
+        {
+          builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    });
+
+    services.Configure<TokenConfiguration>(Configuration.GetSection("CheckTokenMiddleware"));
+    services.Configure<BaseRabbitMqConfig>(Configuration.GetSection(BaseRabbitMqConfig.SectionName));
+    services.Configure<BaseServiceInfoConfig>(Configuration.GetSection(BaseServiceInfoConfig.SectionName));
+
+    services.AddHttpContextAccessor();
+    services
+      .AddControllers()
+      .AddJsonOptions(options =>
+      {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+      })
+      .AddNewtonsoftJson();
+
+    string connStr = ConnectionStringHandler.Get(Configuration);
+
+    services.AddDbContext<GroupServiceDbContext>(options =>
+    {
+      options.UseSqlServer(connStr);
+    });
+
+    services.AddHealthChecks()
+      .AddSqlServer(connStr)
+      .AddRabbitMqCheck();
+
+    services.AddBusinessObjects();
+
+    services.ConfigureMassTransit(_rabbitMqConfig);
+
+    services.AddSwaggerGen(options =>
+    {
+      options.SwaggerDoc($"{Version}", new OpenApiInfo
+      {
+        Version = Version,
+        Title = _serviceInfoConfig.Name,
+        Description = Description
+      });
+
+      options.EnableAnnotations();
+    });
+  }
+
+  public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+  {
+    app.UpdateDatabase<GroupServiceDbContext>();
+
+    app.UseForwardedHeaders();
+
+    app.UseExceptionsHandler(loggerFactory);
+
+    app.UseApiInformation();
+
+    app.UseRouting();
+
+    app.UseMiddleware<TokenMiddleware>();
+
+    app.UseCors(CorsPolicyName);
+
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers().RequireCors(CorsPolicyName);
+
+      endpoints.MapHealthChecks($"/{_serviceInfoConfig.Id}/hc", new HealthCheckOptions
+      {
+        ResultStatusCodes = new Dictionary<HealthStatus, int>
+        {
+          { HealthStatus.Unhealthy, 200 },
+          { HealthStatus.Healthy, 200 },
+          { HealthStatus.Degraded, 200 },
+        },
+        Predicate = check => check.Name != "masstransit-bus",
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+      });
+    });
+
+    app.UseSwagger()
+      .UseSwaggerUI(options =>
+      {
+        options.SwaggerEndpoint($"/swagger/{Version}/swagger.json", $"{Version}");
+      });
+  }
+}
